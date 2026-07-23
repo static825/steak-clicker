9f2843ee55d52c517be3d4f68b3510cbe5aa5cae
static825 <static825@gmail.com>
2026-07-23 21:32:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const steak = document.getElementById("steak");
+
+let currentScale = 1;
+let clickOffset = 0;
+let hovered = false;
+
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+steak.addEventListener("mouseenter", () => {
+    hovered = true;
+});
+
+steak.addEventListener("mouseleave", () => {
+    hovered = false;
+});
+
+steak.addEventListener("click", () => {
+    clickOffset -= 0.1;
+});
+
+function gameLoop() {
+    const targetScale = hovered ? 1.5 : 1;
+
+    clickOffset = lerp(clickOffset, 0, 0.3);
+    currentScale = lerp(currentScale, targetScale + clickOffset, 0.3);
+
+    steak.style.transform = `scale(${currentScale})`;
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
